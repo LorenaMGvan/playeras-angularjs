898f55410bcb12ea37e2f78a20bdcb5707cc4e78
lorena <lorena@salsalab.mx>
2021-11-11 12:32:26 -0600

    añadir imagen 4 y quitar comentarios

--- app/carrito/carrito.module.js
+++ app/carrito/carrito.module.js
@@ -40,129 +40,129 @@ angular.module('shirtApp.carrito', [])
         }     
 
 
-        // $scope.getStatesSelect = (codPai) => {
-        //         FacPaises.getStates().then( function (response) {                          
+         $scope.getStatesSelect = (codPai) => {
+                 FacPaises.getStates().then( function (response) {                          
                         
-        //                 $filter('filter')(response, function(state) {
+                         $filter('filter')(response, function(state) {
 
-        //                         if(state.countryCode == codPai) {                                                                                                                                                         
-        //                                 $scope.statescontryOpc.push(state);        
-        //                                 $scope.user.statecountry = state;                          
-        //                                 return true;
-        //                         }
-        //                 });   
+                                 if(state.countryCode == codPai) {                                                                                                                                                         
+                                         $scope.statescontryOpc.push(state);        
+                                         $scope.user.statecountry = state;                          
+                                         return true;
+                                 }
+                         });   
                                                 
-        //         }).catch(function(error){                                                 
-        //                 $log.info(error);                        
-        //         });                          
-        // }    
+                 }).catch(function(error){                                                 
+                         $log.info(error);                        
+                 });                          
+         }    
 
-        // $scope.getPaisesSelect();
-        // $scope.getStatesSelect("MX");
+         $scope.getPaisesSelect();
+         $scope.getStatesSelect("MX");
 
 
-        // $scope.selectStateUser = () => {        
-        //         //$scope.getStatesSelect();                                                                           
-        // }
+         $scope.selectStateUser = () => {        
+                 $scope.getStatesSelect();                                                                           
+         }
 
-        // $scope.selectCountryUser = () => {                
-        //         $scope.countryPhoneCode = $scope.user.pais.phonecode;                                                 
-        //         $scope.statescontryOpc = [];
-        //         $scope.getStatesSelect($scope.user.pais.isoCode);
-        // }
+         $scope.selectCountryUser = () => {                
+                 $scope.countryPhoneCode = $scope.user.pais.phonecode;                                                 
+                 $scope.statescontryOpc = [];
+                 $scope.getStatesSelect($scope.user.pais.isoCode);
+         }
 
-        // $scope.getTotal = () => {                
-        //         angular.forEach($scope.cart, function(value, key) {
-        //         $scope.total += value.qty * value.subtotal;                        
-        //         });            
-        // }        
+         $scope.getTotal = () => {                
+                 angular.forEach($scope.cart, function(value, key) {
+                 $scope.total += value.qty * value.subtotal;                        
+                 });            
+         }        
 
-        // $scope.removePlayera = function(idPlayera) {                                                
-        //         $filter('filter')($scope.cart, function(item) {
-        //                 if(idPlayera == item.id){                                        
-        //                         $scope.posPlayera = $scope.cart.indexOf(item);
-        //                         $scope.cart.splice($scope.posPlayera, 1 );   
-        //                         $cookies.putObject('cart', $scope.cart);
-        //                         return true;
-        //                 }                
-        //                 return false;
-        //         });                       
-        // }
+         $scope.removePlayera = function(idPlayera) {                                                
+                 $filter('filter')($scope.cart, function(item) {
+                         if(idPlayera == item.id){                                        
+                                 $scope.posPlayera = $scope.cart.indexOf(item);
+                                 $scope.cart.splice($scope.posPlayera, 1 );   
+                                 $cookies.putObject('cart', $scope.cart);
+                                 return true;
+                         }                
+                         return false;
+                 });                       
+         }
 
-        // $scope.addCart = () => {
-        //         $scope.readybuy = true;                
-        // }
+         $scope.addCart = () => {
+                 $scope.readybuy = true;                
+         }
 
-        // $scope.customqty = (product, opc) => {
+         $scope.customqty = (product, opc) => {
                                 
-        //         $filter('filter')($scope.cart, function(item) {
-        //                 if(product.id == item.id){                                        
-        //                         $scope.posPlayera = $scope.cart.indexOf(item);
+                 $filter('filter')($scope.cart, function(item) {
+                         if(product.id == item.id){                                        
+                                 $scope.posPlayera = $scope.cart.indexOf(item);
 
-        //                         if(opc === "plus") {                        
-        //                                 product.qty++;
-        //                                 $scope.cart[$scope.posPlayera].qty = product.qty;
-        //                                 $scope.total += 150; 
-        //                         } else {
-        //                                 if(product.qty > 1) {
-        //                                         product.qty--;
-        //                                         $scope.cart[$scope.posPlayera].qty = product.qty;
-        //                                         $scope.total -= 150; 
-        //                                 } else {
-        //                                         $scope.cart.splice($scope.posPlayera, 1 );                   
-        //                                 }                                                                        
-        //                         }
+                                 if(opc === "plus") {                        
+                                         product.qty++;
+                                         $scope.cart[$scope.posPlayera].qty = product.qty;
+                                         $scope.total += 150; 
+                                 } else {
+                                         if(product.qty > 1) {
+                                                 product.qty--;
+                                                 $scope.cart[$scope.posPlayera].qty = product.qty;
+                                                 $scope.total -= 150; 
+                                         } else {
+                                                 $scope.cart.splice($scope.posPlayera, 1 );                   
+                                         }                                                                        
+                                 }
                                                                                                 
-        //                         $cookies.putObject('cart', $scope.cart);
-        //                         return true;
-        //                 }                
-        //                 return false;
-        //         });                                
+                                 $cookies.putObject('cart', $scope.cart);
+                                 return true;
+                         }                
+                         return false;
+                 });                                
                                 
-        // }         
+         }         
 
-        // $scope.buy = () => {   
-        //         $scope.endbuy = true;                
-        // }   
+         $scope.buy = () => {   
+                 $scope.endbuy = true;                
+         }   
                 
-        // $scope.buttoncancel = () => {
-        //         $scope.readybuy = false;
-        // }
+         $scope.buttoncancel = () => {
+                 $scope.readybuy = false;
+         }
                 
-        // $scope.getTotal();        
+         $scope.getTotal();        
         }]);
 
 
-// app.directive('aGreatEye', function () {
-//         return {
-//                 restrict: 'E',
-//                 replace: true,
-//                 template: '<h1>lidless, wreathed in flame, {{1 + 1}} times</h1>'
-//         };
-// });
+ app.directive('aGreatEye', function () {
+         return {
+                 restrict: 'E',
+                 replace: true,
+                 template: '<h1>lidless, wreathed in flame, {{1 + 1}} times</h1>'
+         };
+ });
 
 
-// app.directive('fieldNumber', function() {
-//                 return {
-//                         require: 'ngModel',
-//                         restrict: 'A',
-//                         link: function(scope, element, attr, ngModel) {
+ app.directive('fieldNumber', function() {
+                 return {
+                         require: 'ngModel',
+                         restrict: 'A',
+                         link: function(scope, element, attr, ngModel) {
                         
-//                                 ngModel.$formatters.push(function (value){     
-//                                         if (value == undefined) return;                                                                
-//                                         let fieldNumber = value.toString().replace(/[^0-9]/g, '');
+                                 ngModel.$formatters.push(function (value){     
+                                         if (value == undefined) return;                                                                
+                                         let fieldNumber = value.toString().replace(/[^0-9]/g, '');
 
-//                                         return  fieldNumber;
-//                                 });
+                                         return  fieldNumber;
+                                 });
                         
-//                                 ngModel.$parsers.push(function (value) {                            
-//                                         let fieldNumber = value.toString().replace(/[^0-9]/g, '');                        
-//                                         ngModel. $setViewValue(fieldNumber);
-//                                         ngModel.$render();
+                                 ngModel.$parsers.push(function (value) {                            
+                                         let fieldNumber = value.toString().replace(/[^0-9]/g, '');                        
+                                         ngModel. $setViewValue(fieldNumber);
+                                         ngModel.$render();
 
-//                                         return fieldNumber;
-//                                 });
-//                                 }
-//                 }
-// });
+                                         return fieldNumber;
+                                 });
+                                 }
+                 }
+ });
 
